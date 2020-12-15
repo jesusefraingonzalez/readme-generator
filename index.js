8a9aafee6450eb2c5dfd6be19ce7cf562a3723ed
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2020-12-15 00:18:17 -0500

    change 'prompt' to 'message' in questions

--- index.js
+++ index.js
@@ -6,61 +6,46 @@ const generateMarkdown = require('./generateMarkdown.js');
 // array of questions for user
 //title, description, installation, usage, license, contributing, tests, questions 
 const questions = [
-{
-    type: "input",
-    name: "title",
-    prompt: "What is the name of your project?"
-},
-{
-    type: "input",
-    name: "description",
-    prompt: "Please type a brief description of your project"
-},
-{
-    type: "input",
-    name: "installation",
-    prompt: "Please give a description on how to install your project"
-},
-{
-    type: "input",
-    name: "usage",
-    prompt: "How will your project be used?"
-},
-{
-    type: "input",
-    name: "license",
-    prompt: "What license do you wish to use?"
-},
-{
-    type: "input",
-    name: "github",
-    prompt: "What is your github username?"
-},
-{
-    type: "input",
-    name: "email",
-    prompt: "What is your email?"
-},
-{
-    type: "input",
-    name: "",
-    prompt: ""
-},
-{
-    type: "input",
-    name: "",
-    prompt: ""
-},
-{
-    type: "input",
-    name: "",
-    prompt: ""
-}
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please type a brief description of your project"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please give a description on how to install your project"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How will your project be used?"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "What license do you wish to use?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName , generateMarkdown(data), error => {
+    fs.writeFile(fileName, generateMarkdown(data), error => {
         if (error) throw error;
         else console.log("Success!");
     })
@@ -68,7 +53,8 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-    writeToFile('README.md' , questions);
+    inq.prompt(questions)
+        .then(response => writeToFile('README.md', response));
 }
 
 // function call to initialize program

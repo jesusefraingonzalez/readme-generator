f2eb1b293711a822eeee5dbbc8ca1ef0ccc99c83
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2020-12-14 23:15:43 -0500

    add dependencies and questions

--- index.js
+++ index.js
@@ -1,6 +1,60 @@
-// array of questions for user
-const questions = [
+// dependencies
+const inq = require('inquirer');
+const fs = require('fs');
 
+// array of questions for user
+//title, description, installation, usage, license, contributing, tests, questions 
+const questions = [
+{
+    type: "input",
+    name: "title",
+    prompt: "What is the name of your project?"
+},
+{
+    type: "input",
+    name: "description",
+    prompt: "Please type a brief description of your project"
+},
+{
+    type: "input",
+    name: "installation",
+    prompt: "Please give a description on how to install your project"
+},
+{
+    type: "input",
+    name: "usage",
+    prompt: "How will your project be used?"
+},
+{
+    type: "input",
+    name: "license",
+    prompt: "What license do you wish to use?"
+},
+{
+    type: "input",
+    name: "github",
+    prompt: "What is your github username?"
+},
+{
+    type: "input",
+    name: "email",
+    prompt: "What is your email?"
+},
+{
+    type: "input",
+    name: "",
+    prompt: ""
+},
+{
+    type: "input",
+    name: "",
+    prompt: ""
+},
+{
+    type: "input",
+    name: "",
+    prompt: ""
+}
 ];
 
 // function to write README file
